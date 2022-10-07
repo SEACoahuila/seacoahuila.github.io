a91b7b59b21ebddd2757edfb6cad0c2a7391e6c1
rene lazalde <rene.lazalde@gmail.com>
2022-10-07 14:27:08 -0500

    Se suben cambios de administracion a el apartado de transparencia

---
+++ secretaria-ejecutiva/transparencia/date.js
@@ -0,0 +1,6 @@
+
+
+
+var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+var f=new Date();
+document.write(" Fecha de última actualización: 04 de " + meses[f.getMonth()] + " del " + f.getFullYear());
