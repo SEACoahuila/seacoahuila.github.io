99f6e84cf177c1e650366aa40b83e1b336a465d9
rene lazalde <rene.lazalde@gmail.com>
2022-10-10 11:53:41 -0500

    Se suben cambios de transparencia fecha de actualización

--- secretaria-ejecutiva/transparencia/date.js
+++ secretaria-ejecutiva/transparencia/date.js
@@ -1,6 +1,44 @@
 
+// var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+// var f=new Date();
 
+// let month = f.getMonth() + 1
+// let day = 1
+//  if (month === 10) {
+//     day = 5
+//  } else if (month === 1 ){
+//     day = 2
+//  } else if (month === 4 ){
+//     day = 3
+//  } else if (month === 7 ){
+//     day = 2
+//  } 
+
+// document.write(" Fecha de última actualización: " + day + " de " + meses[f.getMonth()] + " del " + f.getFullYear());
+
+function cargaDate() {
+	console.log("hola")
 
 var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
 var f=new Date();
-document.write(" Fecha de última actualización: 04 de " + meses[f.getMonth()] + " del " + f.getFullYear());
+
+let month = f.getMonth() + 1
+let day = "01"
+ if (month === 10) {
+    day = "05"
+ } else if (month === 1 ){
+    day = "02"
+ } else if (month === 4 ){
+    day = "03"
+ } else if (month === 7 ){
+    day = "02"
+ } 
+
+let fechaCompleta = (" Fecha de última actualización: " + day + " de " + meses[f.getMonth()] + " del " + f.getFullYear());
+
+	document.getElementById("date").textContent += fechaCompleta
+
+}
+
+
+window.onload=cargaDate;
